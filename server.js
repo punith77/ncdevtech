f8f13914129e759a38af097ab9feb24b13f0672c
Punith <punith.ch@gmail.com>
2019-02-28 10:21:30 -0700

    feat: added cors functionality

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
 const passport = require('passport');
+const cors = require('cors')
 
 
 const auth = require('./routes/api/auth')
@@ -11,6 +12,7 @@ const app = express();
 //Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors())
 
 //DB Config
 const db = require('./config/keys').mongoURI;
